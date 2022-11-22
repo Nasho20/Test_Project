f5737c2d772f46833c5dbf3e3e68940753e24f6e
Nasho20 <118524301+Nasho20@users.noreply.github.com>
2022-11-22 16:41:46 +0100

    Few changes
    Added useState Hooks for the Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Login.css'
 
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+    console.log(username + password)
+
+  }
+
   return (
     <div className='bg-image'>
       <div className='login-box'>
         <h1>Login</h1>
         <h3>Enter your credentials:</h3>
-        <form className='login-form'>
-          <input type="text" placeholder="Username" id="username" /><br />
-          <input type="password" placeholder="Password" id="password" /> <br />
+        <form onSubmit={handleSubmit} className='login-form'>
+          <input value={username} type="text" placeholder="Username" id="username" /><br />
+          <input value={password} type="password" placeholder="Password" id="password" /> <br />
           <input type="checkbox" id="remember-me" name="remember-button" value="Remember" />
           <label for="remember-button">Remember me</label><br />
           <a href="/">Forgot your password?</a> <br />
---
+++ src/components/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/Register.css"
+
+export default function Register() {
+
+    return (
+        <div>
+            <h1>Register</h1>
+            <h3>Complete your profile</h3>
+        </div>
+    )
+}

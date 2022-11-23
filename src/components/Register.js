374d7666827021eeb67224a210620aebfa42d4ab
Nasho20 <thanasbaba@gmail.com>
2022-11-24 00:32:01 +0100

    Added some of the required features/fields in the Resgister form.

--- src/components/Register.js
+++ src/components/Register.js
@@ -11,6 +11,29 @@ export default function Register() {
     const [adress, setAdress] = useState('');
     const [linkedin, setLinkedin] = useState('');
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username);
+    
+      }
+    return (
+        <div className='bg-image'>
+        <div className='login-box'>
+          <h1>Register here</h1>
+          <h3>Enter your Signup credentials:</h3>
+          <form onSubmit={handleSubmit} className='register-form'>
+            <label htmlFor='username'>Email: </label>
+            <input value={username} type="email" placeholder="Username" id="username" onChange={(e) => setUsername(e.target.value)} /><br />
+            <label htmlFor='password'>Password: </label>
+            <input value={password} type="password" placeholder="Password" id="password" onChange={(e) => setPassword(e.target.value)} /> <br />
+            <input type="checkbox" id="remember-me" name="remember-button" value="Remember" />
+            <label for="remember-button">Remember me</label><br />
+            <a href="/">Forgot your password?</a> <br />
+            <button className='signup'>SIGN UP</button><button type='submit'>LOGIN</button>
+          </form>
+        </div>
+      </div>
+    )
 }
 
 

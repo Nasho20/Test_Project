8db7eb71917fbc48bf2884f6085a8eced781c627
Nasho20 <thanasbaba@gmail.com>
2022-12-01 00:04:31 +0100

    Few fixes

--- src/authentication/authProvider.js
+++ src/authentication/authProvider.js
@@ -4,10 +4,58 @@ import {
     AUTH_LOGOUT,
     AUTH_ERROR,
     AUTH_CHECK,
-}  Form 'react-admin';
+}  from 'react-admin';
+import { createBrowserHistroy } from 'history';
+const history = createBrowserHistroy();
+
+const homepage=()=> {
+    history.push('/home'); //kryen redirect te funksionit ne homepage
+}
 
 export default (type, params, props) => {
+
     if (type === AUTH_LOGIN) {
         const { username, password } = params;
+
+        if (username === 'user' && password === 'password') {
+            localStorage.setItem('role', 'user');
+            localStorage.removeItem('not_authenticated');
+            homepage();
+            return Promise.resolve();
+        }
+
+        if (username === 'admin' && password === 'password') {
+            localStorage.setItem('role', 'admin');
+            localStorage.removeItem('not_authenticated');
+            return Promise.resolve();
+        }
+        localStorage.setItem('not_authenticated', true);
+        return Promise.reject();
     }
+
+    if (type === AUTH_LOGOUT) {
+        localStorage.setItem('not_authenticated', true);
+        localStorage.removeItem('role');
+        return Promise.resolve();
+    }
+
+    if (type === AUTH_ERROR) {
+        const { status } = params;
+        return status === 401 || status === 403
+        ? Promise.reject()
+        : Promise.resolve();
+    }
+
+    if (type === AUTH_CHECK) {
+        return localStorage.getItem('not_authenticated')
+        ? Promise.reject()
+        : Promise.resolve();
+    }
+
+    if (type === AUTH_GET_PERMISSIONS) {
+        const role = localStorage.getItem('role');
+        return Promise.resolve(role);
+    }
+
+    return Promise.reject('Unknown method');
 }
---
+++ src/components/HomeLogin.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Login from './Login';
+
+export default HomeLogin() => {
+    return (
+        <div><Login></Login></div>
+    )
+}

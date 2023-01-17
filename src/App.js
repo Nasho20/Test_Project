966b6adaf57b9137bc608620c351846740f80fb5
Nasho20 <thanasbaba@gmail.com>
2023-01-17 22:48:16 +0100

    The Login page and the User page work, but the password condition is missing.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 // import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './components/Login.js'
-import UserProfile from './components/UserProfile';
+// import UserProfile from './components/UserProfile';
 
 function App() {
-  const [token, setToken] = useState();
-  const [users, setUsers] = useState([]);
-
-  const fetchUserData = () => {
-    fetch("https://jsonplaceholder.typicode.com/users") //UserData
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setUsers(data)
-      })
-  }
-
-
-  useEffect(() => {
-    fetchUserData()
-  }, [])
-
-
-  // if(!token) { //kusht per t shfaqur login nqs token esht falsy
-  //   return <Login setToken={setToken} /> //mund te vendosim dhe UserProfile
-  // }
-
+  // const [token, setToken] = useState();
+  // const [users, setUsers] = useState([]);
 
   return (
+    // </div>
     <div>
       <Login />
-      <UserProfile />
-      {users.length > 0 && (
-        <ul>
-          {users.map(user => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      )}
+      {/* <UserProfile /> */}
     </div>
     // <Router>
     //   <Login>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,60 @@
 import React, { useState } from 'react'
 import '../styles/Login.css'
-import { Routes, Route, useNavigate } from 'react-router-dom';
 import UserProfile from './UserProfile';
-
 
 export default function Login() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [showError, setShowError] = useState(false);
+
   const handleSubmit = (e) => {
     e.preventDefault();
   }
 
-  return (
-    <div className='bg-image'>
-      <div className='login-box'>
-        <h1>Login</h1>
-        <h3>Enter your credentials:</h3>
-        <form onSubmit={handleSubmit} className='login-form'>
-          <div className='email'>
-            <label htmlFor='username'>Email: </label>
-            <input value={username} type="email" placeholder="Username" id="username" onChange={(e) => setUsername(e.target.value)} /><br />
-          </div>
-          <div className='password'>
-          <label htmlFor='password'>Password: </label>
-          <input value={password} type="password" placeholder="Password" id="password" onChange={(e) => setPassword(e.target.value)} /> <br />
-          </div>
-          <input type="checkbox" id="remember-me" name="remember-button" value="Remember" />
-          <label for="remember-button">Remember me</label><br />
-          <button type='submit'>LOGIN</button> 
-          
-        </form>
+  const fetchUserData = () => {
+
+    fetch("https://jsonplaceholder.typicode.com/users") //UserData
+      .then(response => response.json())
+      .then(result => {
+
+        for (var i = 0; i <= result.length; i++) {
+          const checkEmail = result[i].email === username;
+          // const checkPassword = result[0].password === password;
+          if (checkEmail) {
+            setLoggedIn(true)
+            setShowError(false)
+            break;
+          } else {
+            setLoggedIn(false)
+            setShowError(true)
+          }
+        }
+      })
+  }
+
+  if (loggedIn) return <UserProfile />
+  else
+    return (
+      <div className='bg-image'>
+        <div className='login-box'>
+          <h1>Login</h1>
+          <h3>Enter your credentials:</h3>
+          <form onSubmit={handleSubmit} className='login-form'>
+            <div className='email'>
+              <label htmlFor='username'>Email: </label>
+              <input value={username} type="email" placeholder="Username" id="username" onChange={(e) => setUsername(e.target.value)} /><br />
+            </div>
+            <div className='password'>
+              <label htmlFor='password'>Password: </label>
+              <input value={password} type="password" placeholder="Password" id="password" onChange={(e) => setPassword(e.target.value)} /> <br />
+            </div>
+            {showError && <h3 >username not found</h3>}
+            <input type="checkbox" id="remember-me" name="remember-button" value="Remember" />
+            <label htmlFor="remember-button">Remember me</label><br />
+            <button type='submit' onClick={fetchUserData}>LOGIN</button>
+          </form>
+        </div>
       </div>
-    </div>
-  )
+    )
 }
